7a85c6cb521b1a3f6f46af7891b0072b0112a2e1
iamggragas <iam.ggragas@example.com>
2021-03-01 20:27:55 +0800

    First Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    private float horizontalInput;
+    private float Speed = 15.0f;
+    private float xRange = 20.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (transform.position.x < -xRange)
+		{
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+		{
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * Speed * horizontalInput);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+		{
+            // Fire projectiles 
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+		}
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    //public int animalIndex;
+    // Start is called before the first frame update
+    public int xRange = 20;
+    private float startDelay = 2f;
+    private float spawnDelay = 1.5f;
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimals", startDelay, spawnDelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (Input.GetKeyDown(KeyCode.S))
+		{
+            SpawnRandomAnimals();
+		}
+    }
+
+    void SpawnRandomAnimals()
+	{
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+        Vector3 spawnPosition = new Vector3(Random.Range(-xRange, xRange), 0, 20);
+        Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
+    }
+}
